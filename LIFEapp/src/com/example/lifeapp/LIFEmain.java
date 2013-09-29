48bbf5e20ece6d6c074eddb770b778dac1670988
martijnlutgens <martijnlutgens@yahoo.com>
2013-09-29 19:11:38 +0200

    3

--- LIFEapp/src/com/example/lifeapp/LIFEmain.java
+++ LIFEapp/src/com/example/lifeapp/LIFEmain.java
@@ -9,15 +9,8 @@ public class LIFEmain extends DroidGap  {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		super.loadUrl("file:///assets/www/index.html");
+		//super.loadUrl("file:///assets/www/index.html");
+		super.loadUrl("http://dev.lifesites.nl/index.php/life-app");
 		
 	}
-//
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.lifemain, menu);
-//		return true;
-//	}
-
 }
